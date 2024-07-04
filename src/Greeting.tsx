7418c04b58de78529b3c3150b80329b180bfa761
YanNaingYan <yannnai2711@gmail.com>
2024-07-04 16:35:17 +0630

    optional

--- src/Greeting.tsx
+++ src/Greeting.tsx
@@ -2,13 +2,14 @@
 
 type nameProps = {
 name:string,
-messageCount:number
+messageCount?:number
 }
 
 const Greeting = (props: nameProps) => {
+  const {messageCount=0} = props
   return (
     <div>
-        <h2>Welcome {props.name}. You have {props.messageCount} unread messages.</h2>
+        <h2>Welcome {props.name}. You have {messageCount} unread messages.</h2>
     </div>
   )
 }
